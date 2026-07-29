5c8dd891f31f993d024bb7a58cb122273424a158
addycode341 <adiparth342@gmail.com>
2026-07-30 00:52:21 +0530

    Fix Firebase production config

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,14 +1,35 @@
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
-initializeApp({
-  credential: cert(serviceAccount)
+const serviceAccount = {
+
+  projectId: process.env.FIREBASE_PROJECT_ID,
+
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+
+};
+
+
+
+if(!admin.apps.length){
+
+admin.initializeApp({
+
+credential: admin.credential.cert(serviceAccount)
+
 });
 
+}
 
-const db = getFirestore();
+
+const db = admin.firestore();
 
 
 export { db };
+
+export default admin;
